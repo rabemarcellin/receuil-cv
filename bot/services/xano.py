e961463ef40812716a319a409b6176ebb4ab36aa
rabemarcellin <marcellinr.rabe@gmail.com>
2024-02-09 17:58:39 +0300

    wip

--- bot/services/xano.py
+++ bot/services/xano.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def create_profile_service(profile):
     url = "https://x8ki-letl-twmt.n7.xano.io/api:9uKtYW-B/profiles"
@@ -15,7 +16,7 @@ def get_cv_content(cv_url: str):
     response = requests.get(url, params=params)
     if response.status_code == 200:
         data = response.json()
-        print(response.content)
+        data = json.load(data)
         print(data)
         cv_content = data.get("response", {}).get("result", {}).get("formatedText")
         print("cv content", cv_content)
