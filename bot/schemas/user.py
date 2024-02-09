4acef98696db0c4970a3a09670d32875fbda1190
rabemarcellin <marcellinr.rabe@gmail.com>
2024-02-09 14:35:11 +0300

    resolve: make image in link https secure for avoid cors cv url param

--- bot/schemas/user.py
+++ bot/schemas/user.py
@@ -8,6 +8,7 @@ cloudinary.config(
     cloud_name=env.get('CLOUDINARY_NAME'),
     api_key=env.get('CLOUDINARY_API_KEY'),
     api_secret=env.get('CLOUDINARY_API_SECRET'),
+    secure=True
 )
 
 class User:
